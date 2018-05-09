ac397e52ba3a436682e7612c3bf94e3d1ce86751
betsysanchez26 <bedesanchezca@ittepic.edu.mx>
2018-05-08 22:36:04 -0600

    s08- Inicio de sesion con Google, solo hay un error pero en teoria ya deberia de funcionar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import  firebase  from "firebase";
+import { GooglePlus } from '@ionic-native/google-plus'; 
+
+export const firebaseConfig={
+  apiKey: "AIzaSyCu0w8ldHTdBLZW-iDA4hDXk9REEHyFd_4",
+    authDomain: "proyecto1-login.firebaseapp.com",
+    databaseURL: "https://proyecto1-login.firebaseio.com",
+    projectId: "proyecto1-login",
+    storageBucket: "proyecto1-login.appspot.com",
+    messagingSenderId: "952784663505"
+}
+
+firebase.initializeApp(firebaseConfig);
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +39,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GooglePlus
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,65 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireModule } from 'angularfire2';
+import  firebase  from "firebase";
+import { GooglePlus } from '@ionic-native/google-plus';
+
+//import { Validators, FormBuilder, FormGroup, FormControl,AbstractControl } from '@angular/forms';
+//import { Validator } from '@angular/forms/src/directives/validators';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+ // loginForm: FormGroup;
+  loginError: string;
+ // private auth: AuthService;
   user:any={
     username:"",
     password:""
   }
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,public googlePlus:GooglePlus) {
+    /*this.loginForm = fb.group({
+			email: ['', Validators.compose([Validators.required, Validators.email])],
+			password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+		});*/
   }
+  /*login() {
+		let data = this.loginForm.value;
 
+		if (!data.email) {
+			return;
+		}
+
+		let credentials = {
+			email: data.email,
+			password: data.password
+    };
+	//	this.auth.signInWithEmail(credentials)
+	//		.then(
+	//			() => this.navCtrl.setRoot(HomePage),
+	//			error => this.loginError = error.message
+	//		);
+	}*/
   succesfullLogin():void{
 
   }
+  loginWithGoogle(){
+    this.googlePlus.login({
+      'webClientID':'952784663505-k0o2fhrt01hehd5hg5n4flgjom490c8d.apps.googleusercontent.com',
+      'ofline':true
+    }).then(res=>{
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+      .then(suc=>{
+        alert("Login Success")
+      }).catch(notsuc=>{
+        alert("Not Success ")
+      })
+    })
+  }
+  signup(){}
 
 }
