e78b2e04f019600f65869ff5a831f736075e7a64
jjesusmonroy <ppchuyjjms@gmail.com>
2018-06-01 00:38:45 -0600

    creo que ya quedo pero no puedo probar por que es apra nativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { AngularFireModule } from "angularfire2";
 import { AngularFireAuthModule } from "angularfire2/auth";
-
+import { GooglePlus } from "@ionic-native/google-plus";
+import firebase from "firebase";
 import { MyApp } from './app.component';
 import { FIREBASE_CONFIG } from "./app.firebase.config";
 
@@ -26,6 +27,7 @@ import { FIREBASE_CONFIG } from "./app.firebase.config";
   providers: [
     StatusBar,
     SplashScreen,
+    GooglePlus,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from "../../models/user";
 import { AngularFireAuth } from "angularfire2/auth";
+import { GooglePlus } from "@ionic-native/google-plus";
+import { AngularFireModule } from "angularfire2";
+import firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -11,7 +14,7 @@ import { AngularFireAuth } from "angularfire2/auth";
 export class LoginPage {
 
   user = {} as User;
-  constructor(private afAuth: AngularFireAuth
+  constructor(private afAuth: AngularFireAuth, public googleplus: GooglePlus
     , public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -19,11 +22,11 @@ export class LoginPage {
     try {
       const result = this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
       console.log(result);
-      if(result){
+      if (result) {
         this.navCtrl.push('HomePage');
       }
     }
-    catch(error){
+    catch (error) {
       console.error(error);
     }
   }
@@ -32,12 +35,22 @@ export class LoginPage {
     this.navCtrl.push('RegisterPage');
   }
 
-  loginFacebook(){
+  loginFacebook() {
 
   }
 
-  loginWithGoogle(){
-    
+  loginWithGoogle() {
+    this.googleplus.login({
+      'webClientId': '1008339185704-e5eaa8gko2ip1haer53t5jpko0b3koe7.apps.googleusercontent.com',
+      'offline': true
+    }).then(res => {
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+        .then(suc => {
+          alert("LOGIN SUCCESFULLY");
+        }).catch(ns => {
+          alert("NOT SUCCESFULL")
+        })
+    });
   }
 
 }
